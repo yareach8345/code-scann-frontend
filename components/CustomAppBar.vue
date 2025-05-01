912f28541de143bcce3511f5ad303c756134da00
yareach8345 <mine8345@naver.com>
2025-05-01 16:35:43 +0900

    feat: login, logout 버튼 추가

--- components/CustomAppBar.vue
+++ components/CustomAppBar.vue
@@ -3,12 +3,17 @@ import { useDisplay } from "vuetify/framework";
 
 const isMobile = ref(false)
 const showNav = inject<Ref<boolean>>("show-nav")
+const logined = ref(false)
 
 if(import.meta.client) {
   const { mobile } = useDisplay()
   watch(mobile, () => {
     isMobile.value = mobile.value
   })
+
+  //todo: 이 후 세션을 다루는 코드 추가할 것
+  const sessionId = useCookie("sessionId")
+  logined.value = sessionId !== undefined
 }
 
 const toggleNavDrawer = () => {
@@ -25,6 +30,14 @@ const toggleNavDrawer = () => {
     <v-app-bar-title>
       Code Snacc
     </v-app-bar-title>
+    <v-btn border="md opacity-100" v-if="logined">
+      <v-icon>mdi-login</v-icon>
+      login
+    </v-btn>
+    <v-btn border="md opacity-100" v-else>
+      <v-icon>mdi-logout</v-icon>
+      logout
+    </v-btn>
   </v-app-bar>
 </template>
 
