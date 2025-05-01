f6e29de60824cd3357d2684531de48efebfcad02
yareach8345 <mine8345@naver.com>
2025-05-01 11:18:09 +0900

    add - default 추가
    - 앞서 만든 app-bar과 navigation drawer를 보이는 레이아웃 default Layout 추가

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-layout>
+    <custom-app-bar/>
+    <custom-navigation-drawer/>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-layout>
+</template>
+
+<style scoped>
+
+</style>
