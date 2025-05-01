500185f8d6c5cf4cbf1c9525eb6b36f76510a01a
yareach8345 <mine8345@naver.com>
2025-05-01 10:57:44 +0900

    add - AppBar 추가
    - 페이지 상단에 표시 할 AppBar추가
    - mobile로 판단될 경우 메뉴 아이콘이 나오도록 설정
        * 이후 NavigationDrawer와 연동 예정

---
+++ components/CustomAppBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useDisplay } from "vuetify/framework";
+
+const isMobile = ref(false)
+if(import.meta.client) {
+  const { mobile } = useDisplay()
+  watch(mobile, () => {
+    isMobile.value = mobile.value
+  })
+}
+</script>
+
+<template>
+  <v-app-bar elevation="0" class="border-b-sm border-opacity-100">
+    <v-expand-x-transition>
+      <v-app-bar-nav-icon v-if="isMobile"/>
+    </v-expand-x-transition>
+    <v-app-bar-title>
+      Code Snacc
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
