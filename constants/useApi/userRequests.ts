313c98c8b29693fcb7b33bc079f390c23661cb8f
yareach8345 <mine8345@naver.com>
2025-05-05 01:43:15 +0900

    add: JoinDto와 유저 확인을 위한 기능 추가
    - JoinDto추가
    - UserExistenceResponse DTO추가
    - userRequests에 checkIdExist 함수 추가

--- constants/useApi/userRequests.ts
+++ constants/useApi/userRequests.ts
@@ -1,4 +1,5 @@
 import type {UserInfo} from "~/model/UserInfo";
+import type {UserExistenceResponse} from "~/dto/user/UserExistenceResponse";
 
 export async function getMyInfo() {
     const { $axios } = useNuxtApp()
@@ -8,4 +9,16 @@ export async function getMyInfo() {
     console.log(`getMyInfoResponse=${JSON.stringify(getMyInfoResponse)}`)
 
     return getMyInfoResponse
+}
+
+export async function checkIdExist(id: string) {
+    const { $axios } = useNuxtApp()
+
+    console.log(`trying check id exist with id=${id}`)
+    const response = await $axios.get<UserExistenceResponse>(`/user/${id}/check-id`)
+
+    const checkIdExistResponse = response.data
+    console.log(`check id exist response=${JSON.stringify(checkIdExistResponse)}`)
+
+    return checkIdExistResponse
 }
---
+++ dto/user/JoinDto.ts
@@ -0,0 +1,5 @@
+export interface JoinDto {
+    id?: string,
+    password?: string,
+    nickName?: string
+}
