9ab95b8da0e33f1617f1248a5373033fc366322c
yareach8345 <mine8345@naver.com>
2025-05-06 13:45:00 +0900

    edit: 테마 설정 수정

---
+++ components/UserAccountIcon.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {useLoginStore} from "~/stores/loginStore";
+
+const store = useLoginStore()
+const { userInfo } = storeToRefs(store)
+</script>
+
+<template>
+  <v-badge location="bottom right" color="success" v-if="userInfo?.role === 'ADMIN'">
+    <v-icon border="thin">{{userInfo?.icon}}</v-icon>
+  </v-badge>
+  <v-icon v-else>{{userInfo?.icon}}</v-icon>
+</template>
+
+<style scoped>
+
+</style>
